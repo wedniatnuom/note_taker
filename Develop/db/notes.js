c1259b93cf2dabaa483210bebd7dcdc47323e317
Jeremy Johnson <jeremyjohnson@Jeremys-Mini.hsd1.ut.comcast.net>
2022-08-28 17:01:22 -0600

    added more routes for notes. Get route to retrieve notes, and put route to submit new notes

---
+++ Develop/db/notes.js
@@ -0,0 +1,12 @@
+const notes =[
+    {
+        title: 'Express.js',
+        text: 'Express allows you to write Get, Post, and Delete routes'
+    },
+    {
+        title: 'Hello World!',
+        text: 'Sample text for a new note.'
+    }
+];
+
+module.exports = notes
--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -2,25 +2,7 @@ const express = require('express');
 const path = require('path');
 const PORT = 3002;
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-const notes = require('./db/notes');
-
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/notes.html'));
-});  
-
-app.get('/api/notes', (req,res) => {
-    console.log(notes);
-    res.status(200).json(notes);
-});
-
 
 app.post('/notes', (req,res) => {
     const { title, text } = req.body;
@@ -43,7 +25,3 @@ app.post('/notes', (req,res) => {
         res.status(500).json('Error in creating new note');
     }
 });
-
-app.listen(PORT, () =>
-  console.log(`Express server listening on port http://localhost:${PORT}!`)
-);
